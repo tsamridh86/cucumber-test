7c7a8d7d92ba599764512cbd9a46d29374f593c7
tsamridh86 <t.samridh.86@gmail.com>
2019-09-11 22:34:45 +0530

    added reporting logic

--- src/test/java/hellocucumber/RunCucumberTest.java
+++ src/test/java/hellocucumber/RunCucumberTest.java
@@ -5,6 +5,8 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"}, glue = {"hooks","hellocucumber"})
+@CucumberOptions(plugin = {"pretty","html:target/reports/html",
+"json:target/reports/json/report.json",
+"junit:target/reports/junit/report.xml"}, glue = {"hooks","hellocucumber"})
 public class RunCucumberTest {
 }
